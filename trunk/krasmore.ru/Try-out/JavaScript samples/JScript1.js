eabdb7001d9450536cd4a34a899cbaab1389d57b
Shal1928 <Shal1928@b3659351-8b52-5b06-418f-a1c84f1d4588>
2012-08-03 03:47:47 +0000

    Add JavaScript Samples

---
+++ trunk/krasmore.ru/Try-out/JavaScript samples/JScript1.js
@@ -0,0 +1,62 @@
+﻿//    var imagePreview = $("<img src='Images/lookMe.png' alt='' class='ttPreview' />").appendTo(".toolTipPlace");
+//    imagePreview.css({ opacity: 0 });
+
+
+//    imagePreview.bind({
+//        mousemove: function (e) 
+//        {
+//            alert("Mouse enter");
+//             for (var key in toolTipPlacesCollection) 
+//             {
+
+//                 var previewTop = toolTipPlacesCollection[key]['y'] - 25;
+//                 var previewBottom = toolTipPlacesCollection[key]['y'] + 25;
+
+//                 if ((e.pageY >= previewTop) && (e.pageY <= previewBottom)) 
+//                 {
+
+//alert("Mouse enter");
+//            $("span.toolTipPlace").each(function () {
+
+//                var idConst = "ttPreviewId";
+//                var x = findPos(this)['x'];
+//                var y = findPos(this)['y'];
+//                var imagePreviewCollection = $("#" + idConst + x + "_" + y);
+
+
+//                if ((y == toolTipPlacesCollection[key]['y']) && (!imagePreviewCollection.length)) {
+//                    $("<img src='Images/lookMe.png' alt='' class='ttPreview' id='" + idConst + x + "_" + y + "' />").appendTo(this);
+////                    var divElement = $("<img width='1px' height='50px' id='ShowTrigger'></img>").appendTo(this);
+////                    $(divElement).hide();
+//                }
+//            });
+//                 }
+//             }
+//        },
+//        mouseleave: function () {
+//            //alert("leave");
+//        }
+
+
+
+//    });
+
+//    .bind("hover",
+//        function () {
+//            alert("enter");
+//        }
+//    );
+
+//areaElement
+//$(divElement).hide();
+
+//    $("span.toolTipPlace").each(function () {
+//        alert(findPos(this)); // выведет содержимое ссылок
+//    });
+
+
+
+
+//    for (var key in toolTipPlacesCollection) {
+//        alert(toolTipPlacesCollection[key]);
+//    }
