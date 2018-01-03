5190a432d9f45d3489003462423f64ca275e5544
Shal1928 <Shal1928@yandex.ru>
2018-01-03 19:08:37 +0700

    add popup style resolve popup for image type

--- krasmore.ru/site/scripts/cell-insta-gallery.js
+++ krasmore.ru/site/scripts/cell-insta-gallery.js
@@ -1,4 +1,6 @@
 var endpointFetchLast = "https://api.instagram.com/v1/users/self/media/recent/?access_token=4277204947.f83003d.4ab80ededfde4db6ac191524204c2faa";
+var THUMBNAIL_INSTA = "thumbnail_insta";
+var records = [];
 
 $(document).ready(function () {
     $.ajax({
@@ -16,9 +18,9 @@ $(document).ready(function () {
                 return;
             }
 
-            generateGallery(response.data.length);
-            var containers = $('div.thumbnail-insta');
-            var records = getRecordsAndFillGallery(response.data, containers);
+            generateGallery(response.data.length - 1);
+            var containers = $('div.' + THUMBNAIL_INSTA);
+            fillRecordsAndFillGallery(response.data, containers);
 
 
         }
@@ -32,14 +34,17 @@ function generateGallery(instaFetchCount) {
     var columns = Math.floor(screenWidth / (thumbWidth + thumbMargin));
     columns = columns > 4 ? 4 : columns;
     var rows = Math.floor(instaFetchCount / columns);
-
-    var i = 1;
+    
+    var i = 0;
     for(var r = 1; r <= rows; r++) {
 
         var rowPointer = $("#insta-gallery").append("<div class='row'></div>");
 
         for(var c = 1; c <= columns; c++) {
-            rowPointer.append("<div class='cell'></div>").append("<div class='thumbnail-insta'></div>");
+            var id = THUMBNAIL_INSTA+"_"+i;
+            rowPointer.append("<div class='cell'></div>").append(
+                "<div data-popup-open='"+i+"' class='"+THUMBNAIL_INSTA+"'></div>");
+
 
             if(i == instaFetchCount) {
                 break;
@@ -51,10 +56,27 @@ function generateGallery(instaFetchCount) {
             break;
         }
     }
+
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var recordIndex = jQuery(this).attr('data-popup-open');
+        var record = records[recordIndex];
+        $('div.popup-img').css("background-image", "url('"+ record.media[0] +"')");
+        $('#caption').text(record.caption);
+        $('div.popup-insta').fadeIn(350);
+
+        e.preventDefault();
+    });
+
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        $('div.popup-insta').fadeOut(350);
+        e.preventDefault();
+    });
 }
 
-function  getRecordsAndFillGallery(datas, containers) {
-    var records = [];
+function fillRecordsAndFillGallery(datas, containers) {
+    records = [];
 
     var i = -1;
     for(var d in datas) {
@@ -68,14 +90,12 @@ function  getRecordsAndFillGallery(datas, containers) {
             caption: r.caption.text,
             media: getMedia(r)
         };
-        
+
         records.push(record);
 
         i++;
         fillGallery(containers[i], record);
     }
-
-    return records;
 }
 
 
